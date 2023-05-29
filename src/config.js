d0d8e861726973103772fa2e29bf42d689a5211a
dibot55 <diebots555@gmail.com>
2023-05-29 11:07:43 -0600

    conexion string updated

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ config();
 
 export default {
   port: process.env.PORT,
-  mongo_uri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/pinter?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0"
+  mongo_uri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/pinter?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.2"
 }
